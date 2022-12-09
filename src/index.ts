dc32d8afa76896eeaea013a51ef340650113f925
DemetrioG <demetrio.gabriel@hotmail.com>
2022-12-09 09:32:51 -0300

    fix: ajuste nas chamadas e logs

--- src/index.ts
+++ src/index.ts
@@ -5,54 +5,119 @@ import getFolder from "./utils/getFolder";
 import getTags from "./utils/getTags";
 import getTeam from "./utils/getTeam";
 import writeIntoDB from "./utils/writeIntoDB";
+import getFields from "./utils/getFields";
+import updateFieldIntoDB from "./utils/updateFieldIntoDB";
 dotenv.config();
 
+const timeZone = { timeZone: "America/Sao_Paulo" };
+
 const refreshAllTasks = async () => {
+  console.log(
+    `Atualizando Tasks ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
   const [pks, data] = await getAllTasks();
-  return writeIntoDB("clickup_tasks", data);
+  await writeIntoDB("clickup_tasks", data);
+  return console.log(
+    `Tasks atualizadas ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
 };
 
 const refreshFolderLists = async () => {
+  console.log(
+    `Atualizando Lista de Pastas ${new Date().toLocaleString(
+      "pt-BR",
+      timeZone
+    )}`
+  );
   const [pks, data] = await getFolderLists();
-  return writeIntoDB("clickup_folder_lists", data);
+  await writeIntoDB("clickup_folder_lists", data);
+  return console.log(
+    `Lista de pastas atualizadas ${new Date().toLocaleString(
+      "pt-BR",
+      timeZone
+    )}`
+  );
 };
 
 const refreshTags = async () => {
+  console.log(
+    `Atualizando Tags ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
   const [pks, data] = await getTags();
-  return writeIntoDB("clickup_tags", data);
+  await writeIntoDB("clickup_tags", data);
+  return console.log(
+    `Tags atualizadas ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
 };
 
 const refreshTeam = async () => {
+  console.log(
+    `Atualizando Time ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
   const [pks, data] = await getTeam();
-  return writeIntoDB("clickup_team", data);
+  await writeIntoDB("clickup_team", data);
+  return console.log(
+    `Time atualizado ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
 };
 
 const refreshFolder = async () => {
+  console.log(
+    `Atualizando Pastas ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
   const [pks, data] = await getFolder();
-  return writeIntoDB("clickup_folders", data);
+  await writeIntoDB("clickup_folders", data);
+  return console.log(
+    `Pastas atualizadas ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
+};
+
+const refreshFields = async () => {
+  console.log(
+    `Atualizando Campos ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
+  const data = await getFields();
+
+  for (const { id, attachments, subtasks } of data) {
+    const params = {
+      UpdateExpression: `SET attachments = :attachments${
+        subtasks ? ", subtasks = :subtasks" : ""
+      }`,
+      ExpressionAttributeValues: {
+        ":attachments": attachments,
+      },
+    };
+
+    if (subtasks) {
+      params.ExpressionAttributeValues[":subtasks"] = subtasks;
+    }
+
+    await updateFieldIntoDB("clickup_tasks", "id", id, params);
+  }
+
+  return console.log(
+    `Campos atualizados ${new Date().toLocaleString("pt-BR", timeZone)}`
+  );
 };
 
 const refreshDatabase = async () => {
-  const timeZone = { timeZone: "America/Sao_Paulo" };
-  console.log(
-    `Atualizando dados ${new Date().toLocaleString("pt-BR", timeZone)}`
-  );
-
-  return Promise.allSettled([
+  const result = await Promise.allSettled([
     refreshAllTasks(),
     refreshFolderLists(),
     refreshTags(),
     refreshTeam(),
     refreshFolder(),
-  ])
-    .then((result) => {
-      const error = result.some(({ status }) => status === "rejected");
-      error
-        ? console.log(result)
-        : console.log(
-            `Dados atualizados ${new Date().toLocaleString("pt-BR", timeZone)}`
-          );
-    })
-    .catch((error) => console.log(error));
+  ]);
+
+  const error = result.some(({ status }) => status === "rejected");
+
+  if (error) {
+    return console.log(result);
+  } else {
+    await refreshFields();
+    return console.log(
+      `Dados atualizados ${new Date().toLocaleString("pt-BR", timeZone)}`
+    );
+  }
 };
 refreshDatabase();
